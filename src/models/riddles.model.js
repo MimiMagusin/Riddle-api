f59c7ce7fb25d3053cf141659c588240a984441a
almozaz <alacruzmozaz@gmail.com>
2017-10-03 14:53:21 +0200

    Add riddles service

--- src/models/riddles.model.js
+++ src/models/riddles.model.js
@@ -1,8 +1,14 @@
+// riddles-model.js - A mongoose model
+//
+// See http://mongoosejs.com/docs/models.html
+// for more of what you can do here.
 module.exports = function (app) {
-  const mongooseClient = app.get('mongooseClient');
+  const mongooseClient = app.get('mongooseClient');;
   const riddles = new mongooseClient.Schema({
     question: { type: String, required: true },
     answer: { type: String, required: true },
+    createdAt: { type: Date, default: Date.now },
+    updatedAt: { type: Date, default: Date.now }
   });
 
   return mongooseClient.model('riddles', riddles);
