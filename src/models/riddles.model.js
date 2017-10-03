871f1353e460217fcb7d788448b0dd03337e99df
almozaz <31137902+almozaz@users.noreply.github.com>
2017-10-03 13:28:35 +0200

    Update riddles.model.js
    Answer is also required

--- src/models/riddles.model.js
+++ src/models/riddles.model.js
@@ -2,7 +2,7 @@ module.exports = function (app) {
   const mongooseClient = app.get('mongooseClient');
   const riddles = new mongooseClient.Schema({
     question: { type: String, required: true },
-    answer: { type: String},
+    answer: { type: String, required: true },
   });
 
   return mongooseClient.model('riddles', riddles);
