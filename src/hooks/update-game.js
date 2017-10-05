2eab96fd59ccd598d276827abbb604c8f8259868
almozaz <alacruzmozaz@gmail.com>
2017-10-05 16:29:50 +0200

    Update game schema

--- src/hooks/update-game.js
+++ src/hooks/update-game.js
@@ -1,10 +1,29 @@
-// Use this hook to manipulate incoming or outgoing data.
-// For more information on hooks see: http://docs.feathersjs.com/api/hooks.html
+const errors = require('feathers-errors');
+const randomRiddle = require('./randomRiddle');
+
+const JOIN_GAME = 'JOIN_GAME';
 
 module.exports = function (options = {}) { // eslint-disable-line no-unused-vars
   return function updateGame (hook) {
     // Hooks can either return nothing or a promise
     // that resolves with the `hook` object for asynchronous operations
-    return Promise.resolve(hook);
+    switch(hook.data.type) {
+
+    case JOIN_GAME :
+      return randomRiddle(hook)
+        .then((riddle) => {
+          hook.data = {
+            '$addToSet': {
+              'playerIds': hook.params.user._id,
+              'playerSymbols': riddle
+            }
+          };
+
+          return Promise.resolve(hook);
+        });
+
+    default :
+      throw new errors.NotAcceptable('Dunno what you want, but you are not gonna get it.');
+    }
   };
 };
