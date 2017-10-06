176b2053c32790d8ca98b4e017fc0eafb83beb6d
almozaz <alacruzmozaz@gmail.com>
2017-10-06 12:00:06 +0200

    Start game button sets game.started to true

--- src/hooks/update-game.js
+++ src/hooks/update-game.js
@@ -2,25 +2,28 @@ const errors = require('feathers-errors');
 const randomRiddle = require('./randomRiddle');
 
 const JOIN_GAME = 'JOIN_GAME';
+const START_GAME = 'START_GAME';
 
 module.exports = function (options = {}) { // eslint-disable-line no-unused-vars
   return function updateGame (hook) {
     // Hooks can either return nothing or a promise
     // that resolves with the `hook` object for asynchronous operations
     switch(hook.data.type) {
-
     case JOIN_GAME :
       return randomRiddle(hook)
         .then((riddle) => {
           hook.data = {
             '$addToSet': {
-              'playerIds': hook.params.user._id,
+              'players.userId': hook.params.user._id,
               'playerSymbols': riddle
             }
           };
 
           return Promise.resolve(hook);
         });
+    case START_GAME :
+      hook.data = { started: true};
+      return hook;
 
     default :
       throw new errors.NotAcceptable('Dunno what you want, but you are not gonna get it.');
